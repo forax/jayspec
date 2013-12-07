ff8cc48f4ea0e1fdc9d27f0d10e83688c507336b
forax <forax@xxx.xxx>
2013-12-07 18:28:53 +0100

    add a way to specify how the check is performed (logging the error, throw an error, etc)

--- jayspec/src/com/github/forax/jayspec/JayAssertion.java
+++ jayspec/src/com/github/forax/jayspec/JayAssertion.java
@@ -1,5 +1,6 @@
 package com.github.forax.jayspec;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.function.DoublePredicate;
 import java.util.function.IntPredicate;
@@ -7,62 +8,93 @@ import java.util.function.LongPredicate;
 import java.util.function.Predicate;
 
 public class JayAssertion {
-  public static class Assert<E> {
-    private final E actual;
+  static class AbstractAssert {
+    final Checker checker;
+
+    AbstractAssert(Checker checker) {
+      this.checker = checker;
+    }
+  }
+  
+  public static class Assert<E> extends AbstractAssert {
+    final E actual;
     
-    Assert(E actual) {
+    Assert(E actual, Checker checker) {
+      super(checker);
       this.actual = actual;
     }
     
-    void check(Predicate<? super E> assertion, String text) {
-      if (!assertion.test(actual)) {
-        throw new AssertionError("Invalid assertion " + String.format(text, String.valueOf(actual)));
-      }
+    void check(Predicate<? super E> assertion, String text){
+      checker.check(actual, assertion, text);
     }
     
     public void isNull() {
-      check(a -> a == null, "%s == null");
+      check(a -> a == null, "== null");
+    }
+    public void isNotNull() {
+      check(a -> a != null, "!= null");
     }
     public void isSameAs(E element) {
-      check(a -> a == element, "%s == " + element);
+      check(a -> a == element, "== " + element);
     }
     public void isNotSameAs(E element) {
-      check(a -> a != element, "%s != " + element);
+      check(a -> a != element, "!= " + element);
     }
     public void isEqualTo(E element) {
-      check(a -> Objects.equals(a, element), "%s equals " + element);
+      check(a -> Objects.equals(a, element), "equals " + element);
     }
     public void isNotEqualTo(E element) {
-      check(a -> !Objects.equals(a, element), "%s not equals " + element);
+      check(a -> !Objects.equals(a, element), "not equals " + element);
     }
   }
   
-  public static class AssertComparable<E extends Comparable<E>> extends Assert<E> {
-    AssertComparable(E actual) {
-      super(actual);
+  public static class AssertComparable<E extends Comparable<? super E>> extends Assert<E> {
+    AssertComparable(E actual, Checker checker) {
+      super(actual, checker);
     }
     
     public void isLessThan(E element) {
-      check(a -> a.compareTo(element) < 0, "%s < " + element);
+      check(a -> a.compareTo(element) < 0, "< " + element);
     }
     public void isLessOrEqualThan(E element) {
-      check(a -> a.compareTo(element) <= 0, "%s <= " + element);
+      check(a -> a.compareTo(element) <= 0, "<= " + element);
     }
     public void isGreaterThan(E element) {
-      check(a -> a.compareTo(element) > 0, "%s > " + element);
+      check(a -> a.compareTo(element) > 0, "> " + element);
     }
     public void isGreaterOrEqualThan(E element) {
-      check(a -> a.compareTo(element) >= 0, "%s >= " + element);
+      check(a -> a.compareTo(element) >= 0, ">= " + element);
     }
     public void isInRange(E first, E last) {
-      check(a -> a.compareTo(first) >= 0 && a.compareTo(last) <= 0, "%s in [" + first + ".." + last + ']');
+      check(a -> a.compareTo(first) >= 0 && a.compareTo(last) <= 0, "in [" + first + ".." + last + ']');
     }
   }
   
-  public static class AssertBoolean {
+  public static class AssertCollection<T, E extends Collection<T>> extends Assert<E> {
+    AssertCollection(E actual, Checker checker) {
+      super(actual, checker);
+    }
+    
+    private <V> void checkSize(V actual, Predicate<? super V> predicate, String text) {
+      check(a -> predicate.test(actual), "size " + text);
+    }
+    
+    public void isEmpty() {
+      check(a -> a.isEmpty(), "is empty");
+    }
+    public AssertInt size() {
+      return new AssertInt(actual.size(), this::checkSize);
+    }
+    public void contains(Object o) {
+      check(a -> a.contains(o), "contains " + String.valueOf(o));
+    }
+  }
+  
+  public static class AssertBoolean extends AbstractAssert {
     private final boolean actual;
     
-    AssertBoolean(boolean actual) {
+    AssertBoolean(boolean actual, Checker checker) {
+      super(checker);
       this.actual = actual;
     }
     
@@ -71,10 +103,8 @@ public class JayAssertion {
       boolean test(boolean t);
     }
     
-    private void check(BooleanPredicate assertion, String text) {
-      if (!assertion.test(actual)) {
-        throw new AssertionError("Invalid assertion " + String.format(text, String.valueOf(actual)));
-      }
+    private void check(BooleanPredicate predicate, String text){
+      checker.check(actual, predicate::test, text);
     }
     
     public void isTrue() {
@@ -91,17 +121,16 @@ public class JayAssertion {
     }
   }
   
-  public static class AssertInt {
+  public static class AssertInt extends AbstractAssert  {
     private final int actual;
     
-    AssertInt(int actual) {
+    AssertInt(int actual, Checker checker) {
+      super(checker);
       this.actual = actual;
     }
     
-    private void check(IntPredicate assertion, String text) {
-      if (!assertion.test(actual)) {
-        throw new AssertionError("Invalid assertion " + String.format(text, String.valueOf(actual)));
-      }
+    private void check(IntPredicate predicate, String text){
+      checker.check(actual, predicate::test, text);
     }
     
     public void isEqualTo(int element) {
@@ -127,17 +156,16 @@ public class JayAssertion {
     }
   }
   
-  public static class AssertLong {
+  public static class AssertLong extends AbstractAssert {
     private final long actual;
     
-    AssertLong(long actual) {
+    AssertLong(long actual, Checker checker) {
+      super(checker);
       this.actual = actual;
     }
     
-    private void check(LongPredicate assertion, String text) {
-      if (!assertion.test(actual)) {
-        throw new AssertionError("Invalid assertion " + String.format(text, String.valueOf(actual)));
-      }
+    private void check(LongPredicate predicate, String text){
+      checker.check(actual, predicate::test, text);
     }
     
     public void isEqualTo(long element) {
@@ -163,10 +191,11 @@ public class JayAssertion {
     }
   }
   
-  public static class AssertFloat {
+  public static class AssertFloat extends AbstractAssert {
     private final float actual;
     
-    AssertFloat(float actual) {
+    AssertFloat(float actual, Checker checker) {
+      super(checker);
       this.actual = actual;
     }
     
@@ -175,10 +204,8 @@ public class JayAssertion {
       boolean test(float t);
     }
     
-    private void check(FloatPredicate assertion, String text) {
-      if (!assertion.test(actual)) {
-        throw new AssertionError("Invalid assertion " + String.format(text, String.valueOf(actual)));
-      }
+    private void check(FloatPredicate predicate, String text){
+      checker.check(actual, predicate::test, text);
     }
     
     public void isEqualTo(float element) {
@@ -204,17 +231,16 @@ public class JayAssertion {
     }
   }
   
-  public static class AssertDouble {
+  public static class AssertDouble extends AbstractAssert {
     private final double actual;
     
-    AssertDouble(double actual) {
+    AssertDouble(double actual, Checker checker) {
+      super(checker);
       this.actual = actual;
     }
     
-    private void check(DoublePredicate assertion, String text) {
-      if (!assertion.test(actual)) {
-        throw new AssertionError("Invalid assertion " + String.format(text, String.valueOf(actual)));
-      }
+    private void check(DoublePredicate predicate, String text){
+      checker.check(actual, predicate::test, text);
     }
     
     public void isEqualTo(double element) {
@@ -239,32 +265,51 @@ public class JayAssertion {
       check(a -> a >= first && a <= last, "%s in [" + first + ".." + last + ']');
     }
   }
-
+  
+  public interface Checker {
+    <E> void check(E actual, Predicate<? super E> assertion, String text);
+  }
+  
+  private final Checker checker;
+  
+  public JayAssertion(Checker checker) {
+    this.checker = checker;
+  }
+  public JayAssertion() {
+    this(JayAssertion::checkAssertion);
+  }
+  
+  private static <E> void checkAssertion(E actual, Predicate<? super E> predicate, String text) {
+    if (predicate.test(actual)) {
+      throw new AssertionError("Invalid assertion " + actual + ' ' + text);
+    }
+  }
+  
   public AssertBoolean that(boolean actual) {
-    return new AssertBoolean(actual);
+    return new AssertBoolean(actual, checker);
   }
   
   public AssertInt that(int actual) {
-    return new AssertInt(actual);
+    return new AssertInt(actual, checker);
   }
   
   public AssertLong that(long actual) {
-    return new AssertLong(actual);
+    return new AssertLong(actual, checker);
   }
   
   public AssertFloat that(float actual) {
-    return new AssertFloat(actual);
+    return new AssertFloat(actual, checker);
   }
   
   public AssertDouble that(double actual) {
-    return new AssertDouble(actual);
+    return new AssertDouble(actual, checker);
   }
 
   public <E> Assert<E> that(E actual) {
-    return new Assert<>(actual);
+    return new Assert<>(actual, checker);
   }
   
   public <E extends Comparable<E>> AssertComparable<E> that(E actual) {
-    return new AssertComparable<>(actual);
+    return new AssertComparable<>(actual, checker);
   }
 }

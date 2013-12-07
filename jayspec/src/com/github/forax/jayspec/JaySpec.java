76788df57c43436422e821025caa86e185d143ac
Samy Dindane <samy@dindane.com>
2013-12-07 11:36:10 +0100

    Specify imported classes

--- jayspec/src/com/github/forax/jayspec/JaySpec.java
+++ jayspec/src/com/github/forax/jayspec/JaySpec.java
@@ -1,6 +1,8 @@
 package com.github.forax.jayspec;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 

bd8ea362a2f667c900f37e1a633a9e9c9d730040
forax <forax@xxx.xxx>
2013-12-08 01:30:30 +0100

    add support for collections in JayAssertion

--- jayspec/examples/src/ExampleTest.java
+++ jayspec/examples/src/ExampleTest.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 import com.github.forax.jayspec.JaySpec;
 
@@ -10,7 +11,7 @@ public interface ExampleTest {
           ArrayList<String> list = new ArrayList<>();
           
           it.should("has a size == 0", verify -> {
-            verify.that(list.size()).isEqualTo(0);
+            verify.that(list).size().isEqualTo(0);
           });
         });
         
@@ -19,15 +20,29 @@ public interface ExampleTest {
           list.add("hello");
           
           it.should("has a size == 1", verify -> {
-            verify.that(list.size()).isEqualTo(1);
+            verify.that(list).size().isEqualTo(1);
           });
           
           it.should("gets the item at index 0", verify -> {
-            verify.that(list.get(0)).isEqualTo("hello");
+            verify.that(list).get(0).isEqualTo("hello");
           });
           
-          it.should("not returns -1 for a different item", verify -> {
-            verify.that(list.indexOf("not hello")).isEqualTo(-1);
+          it.should("not returns -1 as an index of an item not present", verify -> {
+            verify.that(list).indexOf("not hello").isEqualTo(-1);
+          });
+        });
+      });
+      
+      describe(TreeSet.class, it -> {
+        given("a set with one element", () -> {
+          TreeSet<String> set = new TreeSet<>();
+          set.add("hello");
+          
+          it.should("first is the element", verify -> {
+            verify.that(set).first().isEqualTo("hello");
+          });
+          it.should("last is the element", verify -> {
+            verify.that(set).last().isEqualTo("hello");
           });
         });
       });
--- jayspec/src/com/github/forax/jayspec/JayAssertion.java
+++ jayspec/src/com/github/forax/jayspec/JayAssertion.java
@@ -1,6 +1,9 @@
 package com.github.forax.jayspec;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.NavigableSet;
 import java.util.Objects;
 import java.util.function.DoublePredicate;
 import java.util.function.IntPredicate;
@@ -29,22 +32,22 @@ public class JayAssertion {
     }
     
     public void isNull() {
-      check(a -> a == null, "== null");
+      check(a -> a == null, "%s == null");
     }
     public void isNotNull() {
-      check(a -> a != null, "!= null");
+      check(a -> a != null, "%s != null");
     }
     public void isSameAs(E element) {
-      check(a -> a == element, "== " + element);
+      check(a -> a == element, "%s == " + element);
     }
     public void isNotSameAs(E element) {
-      check(a -> a != element, "!= " + element);
+      check(a -> a != element, "%s != " + element);
     }
     public void isEqualTo(E element) {
-      check(a -> Objects.equals(a, element), "equals " + element);
+      check(a -> Objects.equals(a, element), "%s equals " + element);
     }
     public void isNotEqualTo(E element) {
-      check(a -> !Objects.equals(a, element), "not equals " + element);
+      check(a -> !Objects.equals(a, element), "%s not equals " + element);
     }
   }
   
@@ -54,19 +57,19 @@ public class JayAssertion {
     }
     
     public void isLessThan(E element) {
-      check(a -> a.compareTo(element) < 0, "< " + element);
+      check(a -> a.compareTo(element) < 0, "%s < " + element);
     }
     public void isLessOrEqualThan(E element) {
-      check(a -> a.compareTo(element) <= 0, "<= " + element);
+      check(a -> a.compareTo(element) <= 0, "%s <= " + element);
     }
     public void isGreaterThan(E element) {
-      check(a -> a.compareTo(element) > 0, "> " + element);
+      check(a -> a.compareTo(element) > 0, "%s > " + element);
     }
     public void isGreaterOrEqualThan(E element) {
-      check(a -> a.compareTo(element) >= 0, ">= " + element);
+      check(a -> a.compareTo(element) >= 0, "%s >= " + element);
     }
     public void isInRange(E first, E last) {
-      check(a -> a.compareTo(first) >= 0 && a.compareTo(last) <= 0, "in [" + first + ".." + last + ']');
+      check(a -> a.compareTo(first) >= 0 && a.compareTo(last) <= 0, "%s in [" + first + ".." + last + ']');
     }
   }
   
@@ -76,17 +79,82 @@ public class JayAssertion {
     }
     
     private <V> void checkSize(V actual, Predicate<? super V> predicate, String text) {
-      check(a -> predicate.test(actual), "size " + text);
+      check(a -> predicate.test(actual), "size of " + text);
+    }
+    private <V> void checkFirst(V actual, Predicate<? super V> predicate, String text) {
+      check(a -> predicate.test(actual), "first of " + text);
     }
     
     public void isEmpty() {
-      check(a -> a.isEmpty(), "is empty");
+      check(a -> a.isEmpty(), "%s is empty");
     }
     public AssertInt size() {
       return new AssertInt(actual.size(), this::checkSize);
     }
     public void contains(Object o) {
-      check(a -> a.contains(o), "contains " + String.valueOf(o));
+      check(a -> a.contains(o), "%s contains " + o);
+    }
+    public void containsAll(Object... objects) {
+      containsAll(Arrays.asList(objects));
+    }
+    public void containsAll(Collection<?> objects) {
+      check(a -> a.containsAll(objects), "%s contains all" + objects);
+    }
+    public Assert<T> first() {
+      return new Assert<>(actual.iterator().next(), this::checkFirst);
+    }
+  }
+  
+  public static class AssertList<T, E extends List<T>> extends AssertCollection<T, E> {
+    AssertList(E actual, Checker checker) {
+      super(actual, checker);
+    }
+    
+    private <V> void checkIndexOf(V actual, Predicate<? super V> predicate, String text) {
+      check(a -> predicate.test(actual), "index of " + text);
+    }
+    private <V> void checkLastIndexOf(V actual, Predicate<? super V> predicate, String text) {
+      check(a -> predicate.test(actual), "last index of " + text);
+    }
+    private <V> void checkGet(V actual, Predicate<? super V> predicate, String text) {
+      check(a -> predicate.test(actual), "get object at " + text);
+    }
+    private <V> void checkLast(V actual, Predicate<? super V> predicate, String text) {
+      check(a -> predicate.test(actual), "last of " + text);
+    }
+    
+    public Assert<T> get(int index) {
+      return new Assert<>(actual.get(index), this::checkGet);
+    }
+    public AssertInt indexOf(Object object) {
+      return new AssertInt(actual.indexOf(object), this::checkIndexOf);
+    }
+    public AssertInt lastIndexOf(Object object) {
+      return new AssertInt(actual.indexOf(object), this::checkLastIndexOf);
+    }
+    public Assert<T> last() {
+      return new Assert<>(actual.listIterator(actual.size()).previous(), this::checkLast);
+    }
+  }
+  
+  public static class AssertNavigableSet<T, E extends NavigableSet<T>> extends AssertCollection<T, E> {
+    AssertNavigableSet(E actual, Checker checker) {
+      super(actual, checker);
+    }
+    
+    private <V> void checkFirst(V actual, Predicate<? super V> predicate, String text) {
+      check(a -> predicate.test(actual), "first of " + text);
+    }
+    private <V> void checkLast(V actual, Predicate<? super V> predicate, String text) {
+      check(a -> predicate.test(actual), "last of " + text);
+    }
+    
+    @Override
+    public Assert<T> first() {
+      return new Assert<>(actual.first(), this::checkFirst);
+    }
+    public Assert<T> last() {
+      return new Assert<>(actual.last(), this::checkLast);
     }
   }
   
@@ -108,16 +176,16 @@ public class JayAssertion {
     }
     
     public void isTrue() {
-      check(a -> a == true, "== true");
+      check(a -> a == true, "%s == true");
     }
     public void isFalse() {
-      check(a -> a == false, "== false");
+      check(a -> a == false, "%s == false");
     }
     public void isEqualTo(boolean element) {
-      check(a -> a == element, "== " + element);
+      check(a -> a == element, "%s == " + element);
     }
     public void isNotEqualsTo(boolean element) {
-      check(a -> a != element, "!= " + element);
+      check(a -> a != element, "%s != " + element);
     }
   }
   
@@ -134,25 +202,25 @@ public class JayAssertion {
     }
     
     public void isEqualTo(int element) {
-      check(a -> a == element, "== " + element);
+      check(a -> a == element, "%s == " + element);
     }
     public void isNotEqualsTo(int element) {
-      check(a -> a != element, "!= " + element);
+      check(a -> a != element, "%s != " + element);
     }
     public void isLessThan(int element) {
-      check(a -> a < element, "< " + element);
+      check(a -> a < element, "%s < " + element);
     }
     public void isLessOrEqualThan(int element) {
-      check(a -> a <= element, "<= " + element);
+      check(a -> a <= element, "%s <= " + element);
     }
     public void isGreaterThan(int element) {
-      check(a -> a > element, "> " + element);
+      check(a -> a > element, "%s > " + element);
     }
     public void isGreaterOrEqualThan(int element) {
-      check(a -> a >= element, ">= " + element);
+      check(a -> a >= element, "%s >= " + element);
     }
     public void isInRange(int first, int last) {
-      check(a -> a >= first && a <= last, "in [" + first + ".." + last + ']');
+      check(a -> a >= first && a <= last, "%s in [" + first + ".." + last + ']');
     }
   }
   
@@ -169,25 +237,25 @@ public class JayAssertion {
     }
     
     public void isEqualTo(long element) {
-      check(a -> a == element, "== " + element);
+      check(a -> a == element, "%s == " + element);
     }
     public void isNotEqualsTo(long element) {
-      check(a -> a != element, "!= " + element);
+      check(a -> a != element, "%s != " + element);
     }
     public void isLessThan(long element) {
-      check(a -> a < element, "< " + element);
+      check(a -> a < element, "%s < " + element);
     }
     public void isLessOrEqualThan(long element) {
-      check(a -> a <= element, "<= " + element);
+      check(a -> a <= element, "%s <= " + element);
     }
     public void isGreaterThan(long element) {
-      check(a -> a > element, "> " + element);
+      check(a -> a > element, "%s > " + element);
     }
     public void isGreaterOrEqualThan(long element) {
-      check(a -> a >= element, ">= " + element);
+      check(a -> a >= element, "%s >= " + element);
     }
     public void isInRange(long first, long last) {
-      check(a -> a >= first && a <= last, "in [" + first + ".." + last + ']');
+      check(a -> a >= first && a <= last, "%s in [" + first + ".." + last + ']');
     }
   }
   
@@ -209,25 +277,25 @@ public class JayAssertion {
     }
     
     public void isEqualTo(float element) {
-      check(a -> a == element, "== " + element);
+      check(a -> a == element, "%s == " + element);
     }
     public void isNotEqualsTo(float element) {
-      check(a -> a != element, "!= " + element);
+      check(a -> a != element, "%s != " + element);
     }
     public void isLessThan(float element) {
-      check(a -> a < element, "< " + element);
+      check(a -> a < element, "%s < " + element);
     }
     public void isLessOrEqualThan(float element) {
-      check(a -> a <= element, "<= " + element);
+      check(a -> a <= element, "%s <= " + element);
     }
     public void isGreaterThan(float element) {
-      check(a -> a > element, "> " + element);
+      check(a -> a > element, "%s > " + element);
     }
     public void isGreaterOrEqualThan(float element) {
-      check(a -> a >= element, ">= " + element);
+      check(a -> a >= element, "%s >= " + element);
     }
     public void isInRange(float first, float last) {
-      check(a -> a >= first && a <= last, "in [" + first + ".." + last + ']');
+      check(a -> a >= first && a <= last, "%s in [" + first + ".." + last + ']');
     }
   }
   
@@ -244,25 +312,25 @@ public class JayAssertion {
     }
     
     public void isEqualTo(double element) {
-      check(a -> a == element, "== " + element);
+      check(a -> a == element, "%s == " + element);
     }
     public void isNotEqualsTo(double element) {
-      check(a -> a != element, "!= " + element);
+      check(a -> a != element, "%s != " + element);
     }
     public void isLessThan(double element) {
-      check(a -> a < element, "< " + element);
+      check(a -> a < element, "%s < " + element);
     }
     public void isLessOrEqualThan(double element) {
-      check(a -> a <= element, "<= " + element);
+      check(a -> a <= element, "%s <= " + element);
     }
     public void isGreaterThan(double element) {
-      check(a -> a > element, "> " + element);
+      check(a -> a > element, "%s > " + element);
     }
     public void isGreaterOrEqualThan(double element) {
-      check(a -> a >= element, ">= " + element);
+      check(a -> a >= element, "%s >= " + element);
     }
     public void isInRange(double first, double last) {
-      check(a -> a >= first && a <= last, "in [" + first + ".." + last + ']');
+      check(a -> a >= first && a <= last, "%s in [" + first + ".." + last + ']');
     }
   }
   
@@ -281,7 +349,7 @@ public class JayAssertion {
   
   private static <E> void checkAssertion(E actual, Predicate<? super E> predicate, String text) {
     if (!predicate.test(actual)) {
-      throw new AssertionError("Invalid assertion " + actual + ' ' + text);
+      throw new AssertionError("Invalid assertion, " + String.format(text, actual));
     }
   }
   
@@ -311,5 +379,17 @@ public class JayAssertion {
   
   public <E extends Comparable<E>> AssertComparable<E> that(E actual) {
     return new AssertComparable<>(actual, checker);
+  }
+  
+  public <T, E extends Collection<T>> AssertCollection<T, E> that(E actual) {
+    return new AssertCollection<>(actual, checker);
+  }
+  
+  public <T, E extends List<T>> AssertList<T, E> that(E actual) {
+    return new AssertList<>(actual, checker);
+  }
+  
+  public <T, E extends NavigableSet<T>> AssertNavigableSet<T, E> that(E actual) {
+    return new AssertNavigableSet<>(actual, checker);
   }
 }
